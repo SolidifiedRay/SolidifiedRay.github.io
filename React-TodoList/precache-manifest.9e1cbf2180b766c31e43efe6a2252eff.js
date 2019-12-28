a712c6f7235f270de71f919f032fb00dbac542e4
SolidifiedRay <ray2147483647@outlook.com>
2019-12-28 18:46:43 +0800

    react todo list

---
+++ React-TodoList/precache-manifest.9e1cbf2180b766c31e43efe6a2252eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df7e352c2b63f419dbf0919048b748d",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "3a37a7da77f59c6c009f",
+    "url": "/React-TodoList/static/css/main.a0ed3595.chunk.css"
+  },
+  {
+    "revision": "a5fdb85f6f31b6ddfcbb",
+    "url": "/React-TodoList/static/js/2.6dcc1d5c.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/React-TodoList/static/js/2.6dcc1d5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a37a7da77f59c6c009f",
+    "url": "/React-TodoList/static/js/main.9656b1f8.chunk.js"
+  },
+  {
+    "revision": "8e1d4bcd798479a33e0d",
+    "url": "/React-TodoList/static/js/runtime-main.f04ce12f.js"
+  }
+]);
