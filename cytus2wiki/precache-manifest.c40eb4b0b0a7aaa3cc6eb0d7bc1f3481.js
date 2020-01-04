9345ce4f813c870785f16430368aabec3ff68295
SolidifiedRay <ray2147483647@outlook.com>
2020-01-05 04:44:12 +0800

    test song page

---
+++ cytus2wiki/precache-manifest.c40eb4b0b0a7aaa3cc6eb0d7bc1f3481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd20e64fba984d93b9200da70d74ef3",
+    "url": "/cytus2wiki/index.html"
+  },
+  {
+    "revision": "15a64e033539548542ff",
+    "url": "/cytus2wiki/static/css/main.01a843e9.chunk.css"
+  },
+  {
+    "revision": "e943101d99e3bc495947",
+    "url": "/cytus2wiki/static/js/2.b7c2f0e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cytus2wiki/static/js/2.b7c2f0e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15a64e033539548542ff",
+    "url": "/cytus2wiki/static/js/main.4eae683c.chunk.js"
+  },
+  {
+    "revision": "3f9c0e80d869ba07ea54",
+    "url": "/cytus2wiki/static/js/runtime-main.280f01ab.js"
+  }
+]);
