040f873e2222ce267dc951a5e4515b719be5e648
SolidifiedRay <ray2147483647@outlook.com>
2020-01-06 21:49:08 +0800

    test githubpage react router

---
+++ cytus2wiki/precache-manifest.25ec158e153ccb6bd6e95866d956f4f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "febdda4d3f1da143017c7df77a809023",
+    "url": "/cytus2wiki/index.html"
+  },
+  {
+    "revision": "d4d6ff01780c21297e2e",
+    "url": "/cytus2wiki/static/css/main.3d789c56.chunk.css"
+  },
+  {
+    "revision": "e1eb25fafcb34a031483",
+    "url": "/cytus2wiki/static/js/2.3b9056a6.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/cytus2wiki/static/js/2.3b9056a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4d6ff01780c21297e2e",
+    "url": "/cytus2wiki/static/js/main.8a499a09.chunk.js"
+  },
+  {
+    "revision": "3f9c0e80d869ba07ea54",
+    "url": "/cytus2wiki/static/js/runtime-main.280f01ab.js"
+  },
+  {
+    "revision": "e788ce08135387d3128ac17909e1a76d",
+    "url": "/cytus2wiki/static/media/Paff_Logo.e788ce08.png"
+  },
+  {
+    "revision": "2ed905be94890142fa661d0968155b1e",
+    "url": "/cytus2wiki/static/media/body_talk.2ed905be.jpg"
+  },
+  {
+    "revision": "61a3ec6656837d917b6f69525a262eec",
+    "url": "/cytus2wiki/static/media/body_talk.61a3ec66.md"
+  },
+  {
+    "revision": "87dab8d7bfd85db16dfad63340127e9d",
+    "url": "/cytus2wiki/static/media/body_talk.87dab8d7.md"
+  },
+  {
+    "revision": "c03315c8cafafdd8e87b6ba0bd00e3e5",
+    "url": "/cytus2wiki/static/media/body_talk.c03315c8.jpg"
+  },
+  {
+    "revision": "5ce06ae5f3f7095b318a564c44ae48c2",
+    "url": "/cytus2wiki/static/media/survive.5ce06ae5.jpg"
+  },
+  {
+    "revision": "8f46d54d1252792d37bce9dc62a47866",
+    "url": "/cytus2wiki/static/media/survive.8f46d54d.md"
+  }
+]);
